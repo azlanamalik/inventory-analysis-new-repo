203fd96df28e98b85fd1f1994291bd69790cd179
Azlan <ec23400@qmul.ac.uk>
2026-03-01 14:43:33 +0000

    added comment explaining and debug code

--- bestway_new.py
+++ bestway_new.py
@@ -20,7 +20,20 @@ class inventory_management(gym.Env):
         global_cap_scale: float = 500.0,
         max_lead_time: int = 12,
     ):
-        print(sku_table, sku_ids, demand_by_sku)
+        """"
+        Args: 
+            - sku_table: dict mapping sku_id to dict with keys "cost", "price", "holding_cost", "penalty_cost", "fixed_cost", "max_cap", "lead_time"
+            - sku_ids: list of sku_ids used in the environment
+            - demand_by_sku: dict mapping sku_id to numpy array of demand values for each week (use predefines and thenadapt to include the LSTM)
+            - horizon: number of weeks in an episode
+            - demand_window: number of past weeks demand to include in the state
+            - global_*_scale: scaling factors for normalizing the corresponding features in the state and reward
+            - max_lead_time: maximum lead time across all SKUs, used for state representation (can be adapted to be sku specific later)
+
+        """
+        print("sku_table", sku_table
+              , "sku_ids", sku_ids
+              , "demand_by_sku", demand_by_sku)
         super().__init__()
         self.sku_table = sku_table
         self.sku_ids = np.array(sku_ids)
@@ -35,11 +48,11 @@ class inventory_management(gym.Env):
         self.global_fixed_scale = global_fixed_scale
         self.global_cap_scale = global_cap_scale
         self.max_lead_time = max_lead_time
-        # 6 dynamic + 8 context = 14 dims
+        # 6 dynamic + 8 context = 14 dims # see the report for indepth explanation of the state and action space design
         self.obs_dim = 14
         self.act_dim = 1
 
-        # -1 -> 1 as sin and cos domain
+        # -1 -> 1 as sin and cos domain 
         self.observation_space = spaces.Box(
             low=-1.0,
             high=1.0,
